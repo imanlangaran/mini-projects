56cbe99f1b7c5cfdfda61b502f642d8453079482
Iman Langaran <imanlangaran@gmail.com>
2023-11-04 11:11:41 +0330

    edit HW1-1

--- DIP/HW1/1.py
+++ DIP/HW1/1.py
@@ -1,33 +1,10 @@
 import cv2
 
-def convert_grayscale_to_8uint(image):
-  """Converts a grayscale image to 8uint format.
+image = cv2.imread("DIP/HW1/cameraman.jpg")
 
-  Args:
-    image: The grayscale image to convert.
+print(image.dtype)
+# output : uint8
+# with opencv, all the images i've tried are in uint8 format!
 
-  Returns:
-    The converted image in 8uint format.
-  """
-
-  # Convert the image to uint8 format.
-  image = image.astype("uint8")
-
-  # Scale the image to the range [0, 255].
-  image = image * 255
-
-  return image
-
-def main():
-  # Load the image.
-  image = cv2.imread("HW1\cameraman.jpg", cv2.IMREAD_GRAYSCALE)
-
-  # Convert the image to 8uint format.
-  image = convert_grayscale_to_8uint(image)
-
-  # Display the image.
-  cv2.imshow("Image", image)
-  cv2.waitKey(0)
-
-if __name__ == "__main__":
-  main()
+cv2.imshow("Image", image)
+cv2.waitKey(0)
