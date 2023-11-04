dbf948e0e3fb604959fb75e1d4a3d44fe7b7e786
Iman Langaran <imanlangaran@gmail.com>
2023-11-04 12:01:54 +0330

    edit HW1-3

--- DIP/HW1/3.py
+++ DIP/HW1/3.py
@@ -2,58 +2,23 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-def equalize_histogram(image):
-  """Equalizes the histogram of an image.
 
-  Args:
-    image: The image to equalize the histogram of.
+image = cv2.imread("DIP/HW1/cameraman.jpg", cv2.IMREAD_GRAYSCALE)
 
-  Returns:
-    The equalized image.
-  """
+histogram = cv2.calcHist([image], [0], None, [256], [0, 255])
 
-  # Calculate the cumulative histogram.
-  histogram = cv2.calcHist([image], [0], None, [256], [0, 255])
-  cumulative_histogram = np.cumsum(histogram)
+cumulative_histogram = np.cumsum(histogram)
+equalized_image = cv2.LUT(image, cumulative_histogram)
+equalized_histogram = cv2.calcHist([equalized_image], [0], None, [256], [0, 255])
 
-  # Calculate the equalized image.
-  equalized_image = cv2.LUT(image, cumulative_histogram)
+plt.figure()
+plt.plot(histogram)
+plt.xlabel("Intensity")
+plt.ylabel("Count")
 
-  return equalized_image
+plt.figure()
+plt.plot(equalized_histogram)
+plt.xlabel("Intensity")
+plt.ylabel("Count")
 
-def draw_histogram(image):
-  """Draws the histogram of an image.
-
-  Args:
-    image: The image to draw the histogram of.
-
-  Returns:
-    None.
-  """
-
-  # Calculate the histogram.
-  histogram = cv2.calcHist([image], [0], None, [256], [0, 255])
-
-  # Plot the histogram.
-  plt.figure(figsize=(10, 5))
-  plt.plot(histogram)
-  plt.xlabel("Intensity")
-  plt.ylabel("Count")
-  
-
-def main():
-  # Load the image.
-  image = cv2.imread("HW1\cameraman.jpg", cv2.IMREAD_GRAYSCALE)
-
-  # Draw the original histogram.
-  draw_histogram(image)
-
-  # Apply histogram equalization.
-  equalized_image = equalize_histogram(image)
-
-  # Draw the equalized histogram.
-  draw_histogram(equalized_image)
-  plt.show()
-
-if __name__ == "__main__":
-  main()
+plt.show()
