fa6855771f3175f4a0f3ce516ba2f40ff0754766
Iman Langaran <imanlangaran@gmail.com>
2025-02-16 21:58:49 +0330

    fixed it :)

--- DjangoReact/our-quote/src/main.jsx
+++ DjangoReact/our-quote/src/main.jsx
@@ -1,9 +1,20 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Main = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = '/src/js/main.js';
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+};
+
+createRoot(document.getElementById('root')).render(<Main />);
--- DjangoReact/our-quote/vite.config.js
+++ DjangoReact/our-quote/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~bootstrap': 'node_modules/bootstrap',
+    }
+  },
   plugins: [react()],
 })
