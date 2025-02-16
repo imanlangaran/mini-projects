957e78fe36b9700c7fd5aa7d8fc4e8d2e293aade
iman <imanlangaran@gmail.com>
2025-02-16 13:29:01 +0330

    tried to fix fetch

--- DjangoReact/our-quote/src/App.jsx
+++ DjangoReact/our-quote/src/App.jsx
@@ -2,23 +2,71 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [details, setDetails] = useState(null);
+  const [state, setState] = useState({
+    details: [],
+    user: '',
+    quote: '',
+    refresh: false,
+  })
 
   useEffect(() => {
     axios
       .get("http://127.0.0.1:8000/wel/")
       .then((response) => {
         console.log(response.data);
-        setDetails(response.data);
+        const details = response.data;
+        setState((prevState) => ({
+          ...prevState,
+          details: details,
+          refresh: false
+        }));
+        console.log(state);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  },[state.refresh]);
+
+  const renderSwitch = (param) => {
+    switch (param + 1) {
+      case 1:
+        return "primary ";
+      case 2:
+        return "secondary ";
+      case 3:
+        return "success ";
+      case 4:
+        return "danger ";
+      case 5:
+        return "warning ";
+      case 6:
+        return "info ";
+      default:
+        return "yellow ";
+    }
+  }
 
   const handleSubmit = (e) => {
-    console.log(e);
+    e.preventDefault();
+    axios
+    .post('http://127.0.0.1:8000/wel/', {
+      name: state.user,
+      detail: state.quote,
+    })
+    .then((res) =>{
+      console.log(res)
+      setState({user:'',quote:'', refresh:true})
+    })
+    .catch((err) => {console.log(err)})
   };
+
+  const handleInput = (e) => {
+    const {name, value} = e.target;
+    setState({
+      ...state,
+      [name]:value
+    })
+  } 
 
   return (
     <div className="container jumbotron">
@@ -31,10 +79,61 @@ function App() {
               {" "}
             </span>
           </div>
+        
           <input type="text" className="form-control" placeholder="name of Author" aria-label="Username" aria-describedby="basic-addon1" 
-          value={details && details[0].name}/>
+          value={state.user}
+          name="user"
+          onChange={handleInput}/>
         </div>
+
+        <div className="input-group mv-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text">
+              your qoute
+            </span>
+          </div>
+
+          <textarea className="form-control"
+          aria-label="with textarea"
+          placeholder="Tell us what you think of ....."
+          value={state.quote}
+          name="quote"
+          onChange={handleInput}
+          >
+          </textarea>
+        </div>
+        <button className="btn btn-primary mb-5" type="submit">
+          Submit
+        </button>
       </form>
+      <hr
+        style={{
+          color:"#000",
+          backgroundColor:"#000",
+          height: 0.5,
+          borderColor:"#000",
+        }}
+      />
+
+      {state.details && state.details.map((item, index) => (
+        <div key={index}>
+          <div className="card shadow-lg">
+            <div className={"bg-"+renderSwitch(index%6) + " card-body"}>
+              quote {index+1}
+            </div>
+            <div className="card-body">
+              <blockquote className={"text-"+renderSwitch(index % 6) + " blockqoute-footer mb-0"}>
+                <h1> {item.detail} </h1>
+                <footer className="blockquote-footer">
+{" "}
+<cite title="Source Title">{item.name}</cite>
+                </footer>
+              </blockquote>
+            </div>
+          </div>
+          <span className="border border-primary"></span>
+        </div>
+      ))}
     </div>
   );
 }
